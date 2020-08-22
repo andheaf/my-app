bc322a45ba77775814a9ff1d93f00235c03c0a6f
andheaf <andheaf@gmail.com>
2020-08-22 19:40:02 +0700

    Updates

---
+++ precache-manifest.14623d7a30aa279dabba84d0fbba0e91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "671bbbe2d5bb99afccdb2f626513c0ed",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "18decdfe6ac31c324cf6",
+    "url": "/my-app/static/css/main.d65aab1e.chunk.css"
+  },
+  {
+    "revision": "c553ca20d8768bae956c",
+    "url": "/my-app/static/js/2.1c4238c8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-app/static/js/2.1c4238c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18decdfe6ac31c324cf6",
+    "url": "/my-app/static/js/main.cd133efb.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
